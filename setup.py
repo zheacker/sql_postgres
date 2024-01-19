fd7cd5588c7f797ce33f6f86c1533e19303bc777
Zac Heacker <zheacker@users.noreply.github.com>
2024-01-18 22:57:50 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repo for the SQL and PostgreSQL Complete Developer Guide on Udemy.',
+    author='zac',
+    license='',
+)
